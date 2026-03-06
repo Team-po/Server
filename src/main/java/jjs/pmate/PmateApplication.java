63c954f14c775f7b9a4e0e4667306a749362f5cf
sh0723 <pshpark12345@naver.com>
2026-03-06 16:22:40 +0900

    initial commit

---
+++ src/main/java/jjs/pmate/PmateApplication.java
@@ -0,0 +1,13 @@
+package jjs.pmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjs/pmate/PmateApplicationTests.java
@@ -0,0 +1,13 @@
+package jjs.pmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
