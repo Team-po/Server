29f7ad053e2545ee0d37f92158c2f5aff3fbd333
daeun <jaeunda@gmail.com>
2026-04-01 21:53:40 +0900

    fix: make projectTitle optional #9

--- src/main/java/team/po/feature/match/dto/ProjectRequestDto.java
+++ src/main/java/team/po/feature/match/dto/ProjectRequestDto.java
@@ -7,7 +7,6 @@ import team.po.feature.match.enums.Role;
 public record ProjectRequestDto(
         @NotNull(message = "역할 선택은 필수입니다.")
         Role role,
-        @NotBlank(message = "프로젝트 제목 입력은 필수입니다.")
         String projectTitle,
         String projectDescription,
         String projectMvp
--- src/main/resources/db/migration/V4__create_project_request_schema.sql
+++ src/main/resources/db/migration/V4__create_project_request_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE project_request (
     id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id             BIGINT       NOT NULL,
     role              VARCHAR(255) NOT NULL,
-    project_title       VARCHAR(255) NOT NULL,
+    project_title       VARCHAR(255),
     project_description TEXT,
     project_mvp         TEXT,
     status VARCHAR(255) NOT NULL DEFAULT 'WAITING',
