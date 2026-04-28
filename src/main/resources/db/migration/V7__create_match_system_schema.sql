fe0dcd0da76df42a914330e9d34fe6c8b7bd7db8
daeun <jaeunda@gmail.com>
2026-04-29 01:12:07 +0900

    fix: correct migration SQL syntax #31

--- src/main/resources/db/migration/V7__create_match_system_schema.sql
+++ src/main/resources/db/migration/V7__create_match_system_schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE matching_session
 (
     id  BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL
+    created_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    deleted_at TIMESTAMP(6) NULL
 );
 
 CREATE TABLE matching_member
@@ -11,9 +11,9 @@ CREATE TABLE matching_member
     matching_session_id BIGINT NOT NULL,
     project_request_id BIGINT NOT NULL,
     is_accepted BOOLEAN NULL DEFAULT NULL,
-    created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    responded_at DATETIME(6) NULL,
-    deleted_at DATETIME(6) NULL,
+    created_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    responded_at TIMESTAMP(6) NULL,
+    deleted_at TIMESTAMP(6) NULL,
     CONSTRAINT fk_matching_member_session FOREIGN KEY (matching_session_id) REFERENCES matching_session (id),
-    CONSTRAINT fk_matching_member_project_request FOREIGN KEY (project_request_id) REFERENCES project_request (id),
+    CONSTRAINT fk_matching_member_project_request FOREIGN KEY (project_request_id) REFERENCES project_request (id)
 );
