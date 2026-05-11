3faa77085baa5d1132630bfb3df1c74633078bcd
sh0723 <pshpark12345@naver.com>
2026-05-11 15:10:45 +0900

    Set timeout for RestClientConfig

--- src/main/java/team/po/config/RestClientConfig.java
+++ src/main/java/team/po/config/RestClientConfig.java
@@ -1,14 +1,27 @@
 package team.po.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
 	@Bean
-	public RestClient restClient() {
-		return RestClient.create();
+	public RestClient restClient(
+		@Value("${github.oauth.rest-client.connect-timeout:PT3S}") Duration connectTimeout,
+		@Value("${github.oauth.rest-client.read-timeout:PT5S}") Duration readTimeout
+	) {
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		requestFactory.setConnectTimeout(connectTimeout);
+		requestFactory.setReadTimeout(readTimeout);
+
+		return RestClient.builder()
+			.requestFactory(requestFactory)
+			.build();
 	}
 }
