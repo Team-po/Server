b0ab4431b569c946c9eeeeeceab09be5130d4888
daeun <jaeunda@gmail.com>
2026-05-27 22:52:34 +0900

    feat: generate DevGuide on ProjectGroup creation #67

---
+++ src/main/java/team/po/feature/devguide/event/DevGuideEventHandler.java
@@ -0,0 +1,26 @@
+package team.po.feature.devguide.event;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import team.po.feature.devguide.service.DevGuideService;
+import team.po.feature.projectgroup.event.ProjectGroupCreatedEvent;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DevGuideEventHandler {
+	private final DevGuideService devGuideService;
+
+	@Async
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handle(ProjectGroupCreatedEvent event) {
+		log.info("개발 가이드라인 생성 이벤트 수신: projectGroupId={}", event.projectGroupId());
+
+		devGuideService.generate(event.projectGroupId());
+	}
+}
